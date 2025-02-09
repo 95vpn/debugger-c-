ccbcbef0ddf4d3865fdb6b6293c143dbc377116f
Johnny Herrera <jmjaramillo95@gmail.com>
2025-02-08 23:40:36 -0500

    Herramienta-de-depuracion

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+760afdd41cf7488f52ccd7adbc6e84bf114838e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423465ebe6be1b9ccf3e199708e63d43fe5dcdc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
