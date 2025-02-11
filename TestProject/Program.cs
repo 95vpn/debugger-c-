22431c3df2f83acc7c7802db40fd5ad9a2848091
Johnny Herrera <jmjaramillo95@gmail.com>
2025-02-10 23:49:05 -0500

    Detectar-tipos-de-excepciones especificos

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -208,6 +208,8 @@ int valueEntered;
 string userValue = "two";
 valueEntered = int.Parse(userValue); // FormatException occurs
 */
+
+
 /*
 int[] values1 = { 3, 6, 9, 12, 15, 18, 21 };
 int[] values2 = new int[6];
@@ -215,5 +217,122 @@ int[] values2 = new int[6];
 values2[values1.Length - 1] = values1[values1.Length - 1]; // IndexOutOfRangeException occurs
 */
 
+/*
 object obj = "This is a string";
 int num = (int)obj;
+*/
+/*
+int[] values = null;
+for (int i = 0; i <= 9; i++)
+    values[i] = i * 2;
+
+string? lowCaseString = null;
+Console.WriteLine(lowCaseString.ToUpper());
+*/
+
+/*
+decimal x = 400;
+byte i;
+
+i = (byte)x; // OverflowException occurs
+Console.WriteLine(i);
+*/
+
+/*
+double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+try
+{
+    Console.WriteLine(float1 + float2);
+    Console.WriteLine(number1 / number2);
+}
+catch
+{
+    Console.WriteLine("An exception has been caught");
+}
+*/
+
+/*
+try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    try 
+    {
+        WriteMessage();
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+    }
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+    byte smallNumber;
+
+    try
+    {
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in writeMessage {ex.Message}");
+    }
+
+    checked
+    {
+        try
+        {
+            smallNumber = (byte)number1;
+
+        }
+        catch (OverflowException ex) 
+        {
+            Console.WriteLine($"Exception caught in WriteMessage {ex.Message}");
+        }
+    }
+}
+*/
+
+// inputValues is used to store numeric values entered by a user
+string[] inputValues = new string[]{"three", "9999999999", "0", "2" };
+
+foreach (string inputValue in inputValues)
+{
+    int numValue = 0;
+    try
+    {
+        numValue = int.Parse(inputValue);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid readResult. Please enter a valid number.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number you entered is too large or too small.");
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
