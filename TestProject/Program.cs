6941549338111af6eefb25fc20dc1ecee4a82424
Johnny Herrera <jmjaramillo95@gmail.com>
2025-02-11 22:59:10 -0500

    manejar-excepciones

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -313,6 +313,7 @@ static void WriteMessage()
 }
 */
 
+/*
 // inputValues is used to store numeric values entered by a user
 string[] inputValues = new string[]{"three", "9999999999", "0", "2" };
 
@@ -336,3 +337,80 @@ foreach (string inputValue in inputValues)
         Console.WriteLine(ex.Message);
     }
 }
+*/
+
+
+checked 
+{
+    try
+    {
+        
+        int num1 = int.MaxValue;
+        int num2 = int.MaxValue;
+        int result = num1 + num2;
+
+        Console.WriteLine("Result: " + result);
+    }
+    catch(OverflowException ex)
+    {
+        Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
+    }
+}
+
+
+try
+{ 
+ 
+    string str = null;
+    int length = str.Length;
+    Console.WriteLine("String Length: " + length);
+}
+catch (NullReferenceException ex)
+{
+     Console.WriteLine("Error: The reference is null." + ex.Message);
+}
+    
+
+try
+{
+    int[] numbers = new int[5];
+    numbers[5] = 10;
+}
+catch (IndexOutOfRangeException ex)
+{
+     Console.WriteLine("Error: Index out of range." + ex.Message);
+}
+
+try 
+{
+    int num3 = 10;
+    int num4 = 0;
+    int result2 = num3 / num4;
+} 
+catch  (DivideByZeroException ex)
+{
+    Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+}
+
+Console.WriteLine("Exiting program.");
+    
+
+    
+
+
+
+
+
+
+
+ 
+
+
+
+ 
+
+
+
+
+
+
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2fc2fbb6c00d7147183280ed7711a0e1c2ff5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22431c3df2f83acc7c7802db40fd5ad9a2848091")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
